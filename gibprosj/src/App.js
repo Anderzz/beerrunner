3ce69eabdf90577961f01db3206a04ee2a4efa62
Mikkel Dahl Slettebøe <mikkelsletteboe@yahoo.no>
2022-02-25 09:43:14 +0100

    Storing the map in a useRef for extended usage

--- gibprosj/src/App.js
+++ gibprosj/src/App.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Input from "./input/Input";
 import MapContainer from "./map/Map";
-//import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
-import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
-const MapboxDirections = require("@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions");
 
 function App() {
   return (
--- gibprosj/src/map/Map.js
+++ gibprosj/src/map/Map.js
@@ -1,49 +1,36 @@
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import { useState, useEffect, useRef } from "react";
-import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
-const MapboxDirections = require("@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions");
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYW5kZXJ6IiwiYSI6ImNremZod2Z4MDByNXQydm55NmJtN24yNzgifQ.zR-oZIQ3MYpPVl-mlOtxkw";
-
-function MapContainer() {
   mapboxgl.accessToken = MAPBOX_TOKEN;
 
+function MapContainer() {
+  
+
   const mapContainer = useRef(null);
+  const map = useRef(null);
   const [longitude, setLongitude] = useState(10.4);
   const [latitude, setLatitude] = useState(64);
   const [zoom, setZoom] = useState(10);
 
   // initialize map when componenet mounts
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v9",
       center: [longitude, latitude],
       zoom: zoom,
     });
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-      })
-    );
-    map.on("move", () => {
-      setLongitude(map.getCenter().lng.toFixed(4));
-      setLatitude(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
 
-    const directions = new MapboxDirections({
-      accessToken: MAPBOX_TOKEN,
-      unit: "metric",
-      profile: "mapbox/driving",
+    map.current.on("move", () => {
+      setLongitude(map.current.getCenter().lng.toFixed(4));
+      setLatitude(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
     });
-    map.addControl(directions, "top-left");
 
     // Clean up on unmount
-    return () => map.remove();
+    return () => map.current.remove();
   }, []);
 
   return (
