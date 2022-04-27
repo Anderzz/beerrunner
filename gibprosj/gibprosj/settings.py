bb7368a9488434ab7325385770a2a2f1c41ac0ec
Anders Kjelsrud <akjelsrud@gmail.com>
2022-04-27 16:10:08 +0200

    update readme

--- gibprosj/gibprosj/settings.py
+++ gibprosj/gibprosj/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gib',
         'USER': 'postgres',
-        'PASSWORD': 'gibprosjekt',
+        'PASSWORD': 'hyundai123',
         'HOST': 'localhost',
         'PORT': '',
     }
