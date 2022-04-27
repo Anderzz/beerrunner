aedda2649052c6054cfef1f55b555421c0aea9ea
baglosebastian <baglosebastian@gmail.com>
2022-04-27 16:01:26 +0200

    halla

--- gibprosj/gibprosj/settings.py
+++ gibprosj/gibprosj/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gib',
         'USER': 'postgres',
-        'PASSWORD': 'hyundai123',
+        'PASSWORD': '123qwe123qwe',
         'HOST': 'localhost',
         'PORT': '',
 
